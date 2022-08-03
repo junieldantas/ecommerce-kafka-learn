6f601a929ec09a14d7b1467432a1c82dc6d61f5b
Juniel Dantas <juniel.dantas@gmail.com>
2022-08-03 01:26:54 -0300

    feat: Incluindo Producer e Consumer

--- src/main/java/br/com/dantas/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/dantas/ecommerce/FraudDetectorService.java
@@ -1,2 +1,42 @@
-package br.com.dantas.ecommerce;public class FraudDetectorService {
+package br.com.dantas.ecommerce;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+@Slf4j
+public class FraudDetectorService {
+    //Criando consumidor para o topico
+    public static void main(String[] args) throws InterruptedException {
+        var consumer = new KafkaConsumer<String, String>(properties()); //Inicializa consumer com as configirações properties()
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER")); //realiza subscribe no topico a ser consumido
+        while (true) { //Loop para leitura do topico
+            var records = consumer.poll(Duration.ofMillis(100)); //poll() realiza a leitura e recebe como parametro a duração
+            if (!records.isEmpty()) {
+                log.info("[ORDER - CONSUMER_ORDER_TOPIC] - {} Records Found", records.count()); //total de registros encontrados no tópico
+            }
+            //Imprime os registros encontrados no tópico
+            for (var recordTopic : records) {
+                log.info("---------------------------------");
+                log.info("Processing new order, checking for fraud to key: {} value: {} ", recordTopic.key(), recordTopic.value()); //imprime dados do registro
+                Thread.sleep(5000); //tempo de espera em milisegundos para imprimir proximo registro
+                log.info("Order processed");
+            }
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.22.199.238:9092"); //Ip e Porta do kafka server
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()); //Deserealiza utilizando String
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()); //Deserealiza utilizando String
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName()); //Define group_id para consumo do topico
+        //GROUP_ID_CONFIG - se mais de um serviço utilizar o mesmo group id as mensagens do topico serão distribuidas entre os consumidores de mesmo group id
+        return properties;
+    }
 }
--- src/main/java/br/com/dantas/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/dantas/ecommerce/NewOrderMain.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class NewOrderMain {
+package br.com.dantas.ecommerce;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+public class NewOrderMain {
+
+    // Criando Produtor para publicar mensagem em tópico usando kafka
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties()); //Inicializa o producer com as configurações properties()
+        var key = "ORDER_123"; //Order Number
+        var value = "321,12320"; //Client Number, Valor
+
+        var recordTopic = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value); //Cria Producer para Topico ECOMMERCE_NEW_ORDER
+        //send(p1, p2) - Envia para o tópico, onde p1 é o registro e p2 é um callback a ser executado ao finalizar o envio / get() aguarda o retorno
+        producer.send(recordTopic, (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            log.info("[ORDER - NEW_ORDER_TOPIC] - Success - send New Order {} To Topic {}", key, data.topic());
+        }).get();
+    }
+
+    //Configurações para conexão com o kafka server
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.22.199.238:9092"); //Ip e Porta do Kafka Server
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); //Serializa a chave utilizando String
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); //Serializa o value utilizando String
+        return properties;
+    }
 }
